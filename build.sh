ed1e9951b77505f2859a089bdc5fda95a9e2616c
Dustin Eastway <Dustin.Eastway@Gmail.com>
2018-05-28 17:51:36 -0400

    build: starting files and configuration for rollup

--- build.sh
+++ build.sh
@@ -1,11 +1,14 @@
 # Clean up previous distributions
+rm -rf ngc-out
 rm -rf dist
 
 # Create a variable that points at the Angular compiler
 NGC="node node_modules/.bin/ngc"
+ROLLUP="node node_modules/.bin/rollup"
 
 # Compile the source
 $NGC -p src/tsconfig-build.json
+$ROLLUP ngc-out/tstack-lib.js -f es -o dist/tstack-lib.js
 
 # Move over the package.json file
 cp src/package.json dist/package.json
