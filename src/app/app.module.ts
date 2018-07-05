bde24fc2892e096ee20265acfcccad8f53c13ed6
Dustin M. Eastway <Dustin.Eastway@gmail.com>
2018-07-04 21:12:39 -0400

    feat (docs): add @angular/material to the project and add a main toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
+	imports: [
+		BrowserModule,
+		MatToolbarModule,
+		AppRoutingModule
+	],
 	declarations: [
 		AppComponent
 	],
-	imports: [
-		BrowserModule,
-		AppRoutingModule
-	],
-	providers: [],
 	bootstrap: [ AppComponent ]
 })
 export class AppModule { }
