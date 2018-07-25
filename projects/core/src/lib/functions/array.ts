a14902fae7b6fc4b63db3eed2de95d9ad1c691bb
Dustin M. Eastway <Dustin.Eastway@gmail.com>
2018-07-24 23:07:42 -0400

    feat(core): create an easy sort method in core

--- projects/core/src/lib/functions/array.ts
+++ projects/core/src/lib/functions/array.ts
@@ -1,3 +1,5 @@
+import { CompareProperty } from '../types/compare-property';
+
 import { getValue } from './object';
 
 /**
@@ -37,7 +39,7 @@ export function find<T>(items: T[], valueToFind: any, property?: string): T {
 export function findIndex<T>(items: T[], valueToFind: any, property?: string): number {
 	if (!(items instanceof Array)) { return -1; }
 
-	return items.findIndex((item) => getValue(item, property) === valueToFind);
+	return items.findIndex(item => getValue(item, property) === valueToFind);
 }
 
 /**
@@ -48,4 +50,41 @@ export function findIndex<T>(items: T[], valueToFind: any, property?: string): n
  */
 export function pluck<T = any>(items: any[], property: string): T[] {
 	return (items instanceof Array) ? items.map(i => getValue(i, property)) : [];
+}
+
+export function compareItems<T = any>(item1: T, item2: T, ...compareProperties: (string | CompareProperty)[]): -1 | 0 | 1 {
+	if (item1 === item2) { return 0; }
+
+	// if there are not any compare properties, then compare the full items
+	if (compareProperties.length < 1) { compareProperties = [ '' ]; }
+
+	for (const compareProperty of compareProperties) {
+		// get the values of each item for the current compare property
+		const property = (typeof compareProperty === 'string') ? compareProperty : compareProperty.property;
+		const value1 = getValue(item1, property);
+		const value2 = getValue(item2, property);
+
+		if (value1 === value2) {
+			// if the values are the same, then continue to the next property
+			continue;
+		} else if (value1 === undefined) {
+			// undefined goes at the end of the array (based on JavaScript's default sort)
+			return 1;
+		} else if (value1 === null) {
+			// null goes after everything other than undefined
+			return (value2 === undefined) ? -1 : 1;
+		} else if (value2 == null) {
+			// if value1 is not null or undefined and value2 is, then value2 goes after value1
+			return -1;
+		} else {
+			// if value1 and value2 are not equal or null, then return which one is larger
+			return (value1 > value2) ? 1 : -1;
+		}
+	}
+
+	return 0;
+}
+
+export function sort<T = any>(items: T[], ...sortProperties: (string | CompareProperty)[]): T[] {
+	return items.sort((item1, item2) => compareItems(item1, item2, ...sortProperties));
 }
