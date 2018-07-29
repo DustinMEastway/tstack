4f0e077d4d541d9e693c4cbd5e9b79819a740a4e
Dustin M. Eastway <Dustin.Eastway@gmail.com>
2018-07-29 10:42:57 -0400

    feat(client): create boolean-input decorator

---
+++ projects/client/src/lib/decorators/boolean-input.ts
@@ -0,0 +1,40 @@
+import { coerceBooleanProperty} from '@angular/cdk/coercion';
+import { Input } from '@angular/core';
+
+/**
+ * Property/Accessor decorator that casts input values before setting them (optionally set and id when set is called)
+ * @param castType with a cast method used to convert set values to type the needed type
+ * @param [idConfig] used to set an id when set is called
+ */
+export function BooleanInput(bindingPropertyName?: string): any {
+	return function (target: any, propertyKey: string, descriptor?: PropertyDescriptor): any {
+		// stored object coerced into a boolean value
+		let coercedValue: boolean;
+		// object to store the getter & setter on to return
+		const propertyObject: TypedPropertyDescriptor<boolean> = {
+			enumerable: true,
+			configurable: true
+		};
+
+		// throw an error if a there is a getter without a setter since they will not be able to access the casted object
+		if (descriptor && descriptor.set == null) {
+			console.error('Error: property ' + propertyKey + ' is using @BooleanInput with a getter and without a setter');
+		}
+
+		// if the property has a descriptor, then use its get, otherwise return coerced objected
+		propertyObject.get = (descriptor == null) ? () => { return coercedValue; } : descriptor.get;
+
+		propertyObject.set = (value: any) => {
+			// cast object to the needed type
+			coercedValue = coerceBooleanProperty(value);
+
+			// call the existing setter if there was one
+			if (descriptor && descriptor.set) { descriptor.set(coercedValue); }
+		};
+
+		// apply the input decorator to the object
+		Input(bindingPropertyName)(target, propertyKey, descriptor);
+
+		return propertyObject;
+	};
+}
