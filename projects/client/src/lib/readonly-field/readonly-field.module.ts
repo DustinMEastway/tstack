0a1363796c989f8bafa13449036f481409bf7ef5
Dustin M. Eastway <Dustin.Eastway@gmail.com>
2018-08-10 14:05:01 -0400

    refactor(client): remove unneeded material modules from readonly field module

--- projects/client/src/lib/readonly-field/readonly-field.module.ts
+++ projects/client/src/lib/readonly-field/readonly-field.module.ts
@@ -1,16 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatButtonModule, MatFormFieldModule, MatIconModule, MatInputModule } from '@angular/material';
 
 import { TskReadonlyFieldComponent } from './readonly-field.component';
 
 @NgModule({
 	imports: [
-		CommonModule,
-		MatButtonModule,
-		MatFormFieldModule,
-		MatIconModule,
-		MatInputModule
+		CommonModule
 	],
 	declarations: [
 		TskReadonlyFieldComponent
