d6939b5741c00b45f757edfe418beea1cb45f795
Dustin M. Eastway <deastway@live.com>
2019-02-02 20:40:43 -0500

    feat: improve typing on dialog component content type and move configuration of the component into dialog service

--- projects/client/src/lib/dialog/dialog-config.ts
+++ projects/client/src/lib/dialog/dialog-config.ts
@@ -8,7 +8,7 @@ export interface TskDialogConfig<ContentT = any, ResultT = any> {
 	/** @prop actionButtons to display at the bottom of the page */
 	actionButtons?: (TskDialogActionButton<ResultT> | string)[];
 	/** @prop to display in the center of the dialog */
-	content?: string | string[] | Type<ContentT>;
+	content?: ContentT;
 	/** @prop disables closing the dialog by clicking on the backdrop */
 	disableClose?: boolean;
 	/** @prop title at the top of the dialog */
--- projects/client/src/lib/dialog/dialog.service.ts
+++ projects/client/src/lib/dialog/dialog.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
-import { MatDialog, } from '@angular/material';
+import { MatDialog } from '@angular/material';
+import { Type } from '@tstack/core';
 
+import { TskDialogActionButton } from './dialog-action-button';
 import { TskDialogConfig } from './dialog-config';
 import { TskDialogComponent } from './dialog.component';
 
@@ -18,12 +20,29 @@ export class TskDialogService {
 	 * @method open a new dialog
 	 * @param config used to determine what the dialog should look & behave like
 	 */
-	open<ContentT = any, ResultT = any>(config: TskDialogConfig<ContentT, ResultT>): TskDialogComponent<ContentT, ResultT> {
+	open<ResultT = any>(config: TskDialogConfig<string | string[], ResultT>): TskDialogComponent<string[], ResultT>;
+	open<ContentT = any, ResultT = any>(config: TskDialogConfig<Type<ContentT>, ResultT>): TskDialogComponent<ContentT, ResultT>;
+	open<ResultT = any>(config: TskDialogConfig<any, ResultT>): TskDialogComponent<any, ResultT> {
 		// set configuration properties over default configuration object
 		config = Object.assign({}, TskDialogService.defaultConfig, config);
 
-		const dialogRef = this._matDialog.open<TskDialogComponent<ContentT, ResultT>, any, ResultT>(TskDialogComponent);
-		dialogRef.componentInstance.configure(dialogRef, config as TskDialogConfig<ContentT, ResultT>);
+		const dialogRef = this._matDialog.open<TskDialogComponent<any, ResultT>, any, ResultT>(TskDialogComponent);
+		dialogRef.disableClose = config.disableClose;
+
+		const dialog = dialogRef.componentInstance;
+		dialog.title = config.title;
+		dialog.dialogType = config.type;
+		dialog.createContent((typeof config.content === 'string') ? [ config.content ] : config.content);
+		dialog.actionButtons = (!(config.actionButtons instanceof Array)) ? [] : config.actionButtons.map(button => {
+			if (typeof button === 'string') {
+				return {
+					value: button,
+					viewValue: button
+				} as TskDialogActionButton;
+			} else {
+				return button;
+			}
+		});
 
 		return dialogRef.componentInstance;
 	}
