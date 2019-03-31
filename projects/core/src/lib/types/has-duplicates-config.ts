7ff11c2b4e03989439c32605aaf94d2e0f839db2
Dustin M. Eastway <deastway@live.com>
2019-03-30 23:38:36 -0400

    fix(core): make comparator property optional on types/has-duplicates-config.ts

--- projects/core/src/lib/types/has-duplicates-config.ts
+++ projects/core/src/lib/types/has-duplicates-config.ts
@@ -5,5 +5,5 @@ export interface HasDuplicatesConfig<T = any, K = any> {
 	/** @prop property to pull the value used to compare items from */
 	property?: K;
 	/** @prop comparator function used determine if an item's value is less than, greater than or equal to another item's value */
-	comparator: Comparator<T>;
+	comparator?: Comparator<T>;
 }
