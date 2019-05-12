4a3c597fd40b732a612453dca1e0b1f147695fda
Dustin M. Eastway <dustin.eastway@gmail.com>
2019-05-12 12:28:11 -0400

    refactor(docs): split function processor into functions to code readability

--- tools/docs/processors/function.processor.js
+++ tools/docs/processors/function.processor.js
@@ -1,3 +1,67 @@
+/** gets available call(s) (multiple if the function has overloads) */
+function getCallDocs(functionDoc) {
+	const callDocs = (functionDoc.overloads.length) ? functionDoc.overloads : [ functionDoc ];
+
+	return callDocs.map(callDoc => {
+		// TODO: add doc.typeParameters when it is available on overloads
+		const parameters = callDoc.parameters.join(', ');
+
+		return `function ${functionDoc.name}(${parameters}): ${callDoc.type}` ;
+	});
+}
+
+function getDocsFromContent(docId, docContent) {
+	const parameterSelectors = [ '@parameter', '@param' ];
+	const returnSelectors = [ '@returns', '@return' ];
+	const contentPartSelectors = parameterSelectors.concat(returnSelectors);
+	const contentParts = splitAndKeepMatches(docContent, new RegExp(contentPartSelectors.join('|')));
+
+	const docsWithoutSelectors = contentParts.filter(contentPart =>
+		!contentPartSelectors.some(contentPartSelector => contentPart.startsWith(contentPartSelector))
+	);
+
+	const parameterDocs = removeSelectors(contentParts, parameterSelectors).map(splitParameterDocIntoNameAndDescription);
+
+	const returnDocs = removeSelectors(contentParts, returnSelectors);
+
+	if (returnDocs.length > 1) {
+		console.warn(`Multiple @returns located in JSDoc for doc ${docId}`);
+	}
+
+	return {
+		description: docsWithoutSelectors.join('\n'),
+		parameterDocs,
+		returns: returnDocs[0]
+	};
+}
+
+function removeSelectors(contentParts, selectors) {
+	return (contentParts || []).map(contentPart => ({
+		content: contentPart,
+		selector: selectors.find(selector => contentPart.startsWith(selector))
+	})).filter(contentPart =>
+		contentPart.selector != null
+	).map(contentPart =>
+		contentPart.content.substring(contentPart.selector.length).trim()
+	);
+}
+
+function splitParameterDocIntoNameAndDescription(description) {
+	const parameterNameMatch = /^\[?(\w+)\]?(.*)/.exec(description);
+	let name = '';
+
+	if (parameterNameMatch == null) {
+		console.warn(`Unable to find a parameter name in doc '${docId}' using description: '${description}'`);
+		name = ''
+	} else {
+		name = parameterNameMatch[1];
+		description = name + parameterNameMatch[2];
+	}
+
+	return { name, description };
+}
+
+/** splits a string using a regular expression at the beginning of each match */
 function splitAndKeepMatches(searchString, regex) {
 	if (typeof searchString !== 'string') {
 		return [];
@@ -29,65 +93,16 @@ module.exports = function functionProcessor() {
 		docTypes: [ 'function' ],
 		$process: function(docs) {
 			docs.filter(doc => this.docTypes.includes(doc.docType)).forEach(doc => {
-				const callDocs = (doc.overloads.length)
-					? doc.overloads
-					: [ doc ];
-				const content = doc.overloads.concat(doc).reduce(
-					(currentContent, doc) => currentContent || doc.content,
-					''
+				const docWithContent = [doc].concat(doc.overloads).find(doc =>
+					typeof doc.content === 'string' && doc.content !== ''
 				);
 
-				// get parameter and return docs
-				const parameterSelectors = [ '@parameter', '@param' ];
-				const returnSelectors = [ '@returns', '@return' ];
-				const contentPartSelectors = parameterSelectors.concat(returnSelectors);
-				const contentParts = splitAndKeepMatches(content, new RegExp(contentPartSelectors.join('|')));
+				const {
+					description,
+					parameterDocs,
+					returns
+				} = getDocsFromContent(doc.id, (docWithContent != null) ? docWithContent.content : '');
 
-
-				const parameterDocs = contentParts.map(contentPart => ({
-					content: contentPart,
-					selector: parameterSelectors.find(parameterSelector => contentPart.startsWith(parameterSelector))
-				})).filter(contentPart =>
-					contentPart.selector != null
-				).map(contentPart => {
-					let contentWithoutSelector = contentPart.content.substring(contentPart.selector.length).trim();
-					const parameterNameMatch = /^\[?(\w+)\]?(.*)/.exec(contentWithoutSelector);
-					let parameterName = '';
-
-					if (parameterNameMatch == null) {
-						console.warn(`Unable to find a parameter name in doc '${doc.id}' using description: '${contentWithoutSelector}'`);
-						parameterName = ''
-					} else {
-						parameterName = parameterNameMatch[1];
-						contentWithoutSelector = parameterName + parameterNameMatch[2];
-					}
-
-					return {
-						name: parameterName,
-						description: contentWithoutSelector
-					};
-				});
-
-				const returnDocs = contentParts.map(contentPart => ({
-					content: contentPart,
-					selector: returnSelectors.find(returnSelector => contentPart.startsWith(returnSelector))
-				})).filter(contentPart =>
-					contentPart.selector != null
-				).map(contentPart =>
-					contentPart.content.substring(contentPart.selector.length).trim()
-				);
-
-				if (returnDocs.length > 1) {
-					console.warn(`Multiple @returns located in JSDoc for doc ${doc.id}`);
-				}
-
-				const returns = returnDocs[0];
-
-				const description = contentParts.filter(contentPart =>
-					!contentPartSelectors.some(contentPartSelector => contentPart.startsWith(contentPartSelector))
-				).join('\n');
-
-				// create document data object
 				doc.data = Object.assign({}, doc.data, {
 					title: doc.name,
 					description: description,
@@ -96,12 +111,7 @@ module.exports = function functionProcessor() {
 							title: 'Calls',
 							componentSelector: 'table',
 							data: {
-								rows: callDocs.map(callDoc => {
-									// TODO: add doc.typeParameters when it is available on overloads
-									const parameters = callDoc.parameters.join(', ');
-
-									return `function ${doc.name}(${parameters}): ${callDoc.type}` ;
-								})
+								rows: getCallDocs(doc)
 							}
 						},
 						{
@@ -115,7 +125,7 @@ module.exports = function functionProcessor() {
 						{
 							title: 'Returns',
 							componentSelector: 'markdown',
-							display: returns != null,
+							display: typeof returns === 'string' && returns != '',
 							data: returns
 						}
 					]
