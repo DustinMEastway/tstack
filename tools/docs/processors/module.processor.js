79179b3bb936cdd05889c707c592272af7b58315
Dustin M. Eastway <dustin.eastway@gmail.com>
2019-05-21 07:03:38 -0400

    docs: add global module to module processor

--- tools/docs/processors/module.processor.js
+++ tools/docs/processors/module.processor.js
@@ -24,7 +24,17 @@ module.exports = function moduleProcessor(TYPESCRIPT_DOC_TYPES_TO_RENDER) {
 	return {
 		docTypes: [ 'module' ],
 		$process: function(docs) {
-			docs.filter(doc => this.docTypes.includes(doc.docType)).forEach(doc => {
+			const moduleDocs = docs.filter(doc => this.docTypes.includes(doc.docType));
+			const globalModuleDoc = {
+				id: 'public_api',
+				outputPath: 'index.json',
+				exports: moduleDocs.slice()
+			};
+
+			docs.push(globalModuleDoc);
+			moduleDocs.push(globalModuleDoc);
+
+			moduleDocs.forEach(doc => {
 				const moduleSections = [];
 				TYPESCRIPT_DOC_TYPES_TO_RENDER.forEach(docTypeForSection => {
 					const docsInSection = doc.exports.filter(exportedDoc =>
@@ -41,6 +51,8 @@ module.exports = function moduleProcessor(TYPESCRIPT_DOC_TYPES_TO_RENDER) {
 					sections: moduleSections
 				});
 			});
+
+			return docs;
 		},
 		$runAfter: [ 'functionProcessor', 'outputPathProcessor' ],
 		$runBefore: [ 'renderDocsProcessor' ]
