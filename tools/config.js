a4c9ccb386f3c445b1e42dced7c91c4dac0f9973
Dustin M. Eastway <dustin.eastway@gmail.com>
2019-05-23 21:27:18 -0400

    docs: move generated doc files into src/assets

--- tools/config.js
+++ tools/config.js
@@ -1,7 +1,7 @@
 const { resolve } = require('path');
 
 const ROOT_PATH = resolve(__dirname, '..');
-const DOCS_OUTPUT_PATH = resolve(ROOT_PATH, 'src/generated/docs');
+const DOCS_OUTPUT_PATH = resolve(ROOT_PATH, 'src/assets/generated/docs');
 const PROJECTS_PATH = resolve(ROOT_PATH, 'projects');
 const TEMPLATES_PATH = resolve(ROOT_PATH, 'tools/docs/templates');
 
