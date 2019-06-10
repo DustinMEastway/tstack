565ca8db335abf1929a8e314605bdcd833c0c5f5
Dustin M. Eastway <Dustin.Eastway@gmail.com>
2019-06-10 12:28:53 -0400

    refactor(docs): remove unused code from examples
    remove unused code that was generated by Angular from  examples/ReadonlyFieldAppearanceComponent

--- src/app/examples/client/readonly-field-appearance/readonly-field-appearance.component.ts
+++ src/app/examples/client/readonly-field-appearance/readonly-field-appearance.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-readonly-field-appearance',
   templateUrl: './readonly-field-appearance.component.html',
   styleUrls: ['./readonly-field-appearance.component.scss']
 })
-export class ReadonlyFieldAppearanceComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+export class ReadonlyFieldAppearanceComponent {}
