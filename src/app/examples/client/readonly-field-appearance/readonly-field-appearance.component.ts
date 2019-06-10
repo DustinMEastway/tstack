d2948fb794088708fc3b697f3a6e986d2fcf08a1
Dustin M. Eastway <dustin.eastway@gmail.com>
2019-06-10 17:01:45 -0400

    refactor(docs): fix linting errors to fix the automated build

--- src/app/examples/client/readonly-field-appearance/readonly-field-appearance.component.ts
+++ src/app/examples/client/readonly-field-appearance/readonly-field-appearance.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-readonly-field-appearance',
-  templateUrl: './readonly-field-appearance.component.html',
-  styleUrls: ['./readonly-field-appearance.component.scss']
+	selector: 'app-readonly-field-appearance',
+	templateUrl: './readonly-field-appearance.component.html',
+	styleUrls: ['./readonly-field-appearance.component.scss']
 })
 export class ReadonlyFieldAppearanceComponent {}
