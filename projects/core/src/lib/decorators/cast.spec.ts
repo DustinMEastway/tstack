1c4fe5a16d6285b353709bb4adc82c0f1ad319ac
Dustin M. Eastway <Dustin.Eastway@gmail.com>
2021-06-12 23:00:49 -0400

    core: add decorators/cast

---
+++ projects/core/src/lib/decorators/cast.spec.ts
@@ -0,0 +1,33 @@
+import { ObjectBase } from '../types';
+import { Cast } from './cast';
+
+class FakePropertyClass extends ObjectBase {
+  @Cast(FakePropertyClass)
+  inner?: FakePropertyClass;
+}
+
+class FakeClass extends ObjectBase {
+  @Cast(FakePropertyClass)
+  castProperty?: FakePropertyClass;
+}
+
+let fakeCastObj: FakeClass;
+
+beforeEach(() => {
+  fakeCastObj = new FakeClass();
+  fakeCastObj.castProperty = new FakePropertyClass();
+  fakeCastObj.castProperty.inner = new FakePropertyClass();
+});
+
+test('casting inner properties when assigning new from JSON', () => {
+  // arrange
+  const json = JSON.parse(JSON.stringify(fakeCastObj));
+
+  // act
+  const copiedObj = Object.assign(new FakeClass(), json);
+
+  // assert
+  expect(copiedObj).toBeInstanceOf(FakeClass);
+  expect(copiedObj.castProperty).toBeInstanceOf(FakePropertyClass);
+  expect(copiedObj.castProperty.inner).toBeInstanceOf(FakePropertyClass);
+});
